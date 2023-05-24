4937582fe6dd191fc5cd0ebd7e41bedbfc59886c
Juan Santana <juanka9755@hotmail.com>
2023-05-24 08:27:00 -0500

    03-STUDENT APP
    Creación de primer Controller
    
    Se crean Endpoints
    
    GET users
    GET courses
    
    Se instala gorilla mux

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
 
+	router := mux.NewRouter()
+
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/courses", getCourses).Methods("GET")
+
+	server := http.Server{
+		Handler:      http.TimeoutHandler(router, time.Second*5, "Timeout!"),
+		Addr:         "127.0.0.1:5000",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+
+	time.Sleep(10 * time.Second)
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
+
+func getCourses(w http.ResponseWriter, r *http.Request) {
+
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 }
